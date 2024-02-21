c95635fd0f80a8bcae864bcea5aba17a116385ab
andrewSucd <andrew.sproule@ucdconnect.ie>
2024-02-21 10:09:13 +0000

    created visual board, Constants class, changed some code to reduce space

--- app/Player.java
+++ app/Player.java
@@ -1,10 +1,19 @@
+package app;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+    public Player(String name) {
+        this.name = name;
+    }
     private int score;
+    private String name;
 
     //list of actions done by player
     private List<String> actions = new ArrayList<>();
 
+    public List<String> getActions() {
+        return actions;
+    }
 }
---
+++ module-info.java
@@ -0,0 +1,6 @@
+module assignment{
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens app;
+        }
