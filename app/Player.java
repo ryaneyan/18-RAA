4c9b5573ea9855f2bfe6662db1660ad3cf871dc3
ayeshax <72799248+ayeshax@users.noreply.github.com>
2024-05-05 22:18:48 +0100

    Update Player.java - code cleanup

--- app/Player.java
+++ app/Player.java
@@ -4,17 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    public Player(String name) {
-        this.name = name;
-    }
-
     private static ArrayList<Player> players = new ArrayList<Player>();
     private static int playerCount = 0;
     private int score = 0;
     private String name;
-
-    //list of actions done by player
     private List<String> actions = new ArrayList<>();
+   
+    public Player(String name) {
+        this.name = name;
+    }
 
     public List<String> getActions() {
         return actions;
